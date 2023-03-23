184b0acbe3caf9d219ec04787ef445d4241c2fc5
vyiirus11 <113479165+vyiirus11@users.noreply.github.com>
2023-03-22 23:55:30 -0400

    Update tasmiahelen.py

--- tasmiahelen.py
+++ tasmiahelen.py
@@ -8,6 +8,15 @@ def encode(password):
     return encoded
 
 
+def decode(password):
+    decoded = ''
+    for char in password:
+        digit = int(char)
+        decoded += str(digit - 3 % 10)
+
+    return decoded
+
+
 while True:
     print("Menu\n-------------\n")
     print("1. Encode\n2. Decode\n3. Quit")
@@ -16,6 +25,8 @@ while True:
     if yeah == 1:
         password = int(input("Please enter your password to encode: "))
         print("Your password has been encoded and stored!\n")
+    if yolo == 2:
+        encoded = encode(password)
+        print(f'The encoded password is {encoded}, and the original password is {decode(str(password))}\n')
     if yeah == 3:
         break
-
